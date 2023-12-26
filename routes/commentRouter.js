9cc76423c599a93ae75fa910d21268c1d49fd370
bastosjt <151185735+bastosjt@users.noreply.github.com>
2023-12-26 18:28:31 +0100

    Update commentRouter.js

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -2,3 +2,11 @@ const { Router } = require("express");
 const Comment = require("../models/Comment");
 const checkAuth = require("../middlewares/checkAuth");
 const router = new Router();
+
+router.get("/comments", checkAuth, async (req, res, next) => {
+  try {
+    res.json(await Comment.findAll());
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
