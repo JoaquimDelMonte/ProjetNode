94affd762db7feaea40d579acb5af7ae971a11ed
bastosjt <151185735+bastosjt@users.noreply.github.com>
2023-12-26 17:14:50 +0100

    Update checkAuth.js

--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -1,1 +1,21 @@
 const jwt = require("jsonwebtoken");
+
+moduleexports = function(req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.sendstatus(401);
+  }
+  const [type, broken] = authHeader.split(/\s+/);
+  if(type !== "Bearer"){
+    return res.sendStatus(401);
+  }
+
+  try{
+    const payload = jwt.verify(token, process.env?JWT_SECRET);
+    re.userID = payload.sub;
+    next();
+  }catch(e){
+    res.sendStatus(401);
+    console.error(e);
+  }
+};
