019e08b9be33a222a24b67039db4c8fa930a7fc8
bastosjt <151185735+bastosjt@users.noreply.github.com>
2023-12-26 17:33:53 +0100

    Update eventRouter.js

--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -1,1 +1,5 @@
-const {Router} = require('express');
+const {Router} = require("express");
+const Event = require("./models/Event");
+const checkAuth = require("./middlewares/checkAuth");
+const router = new router();
+
