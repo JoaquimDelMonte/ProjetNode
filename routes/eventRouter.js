177035eec1becd440f8adcb65ee12d51a7e1f0e2
bastosjt <151185735+bastosjt@users.noreply.github.com>
2023-12-26 17:48:40 +0100

    Update eventRouter.js

--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -7,6 +7,14 @@ router.get("/events, async (req, res, next)=> {
            try{
                       res.json(await Event.findAll());
            }catch (err){
-                      res.status(422).json()({error: err.message});
+                      res.status(422).json({error: err.message});
            }
+});
+
+router.post("/events, checkAuth, async (req, res, next)=> {
+            try{
+                       res.status(201).json(await Event.create(req.body));
+            }catch(err){
+                      res.status(422).json({error : err.mesage});
+            }
 });
