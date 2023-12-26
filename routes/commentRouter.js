0292579826f156410607be984cc3db0c0b6b8976
bastosjt <151185735+bastosjt@users.noreply.github.com>
2023-12-26 18:33:44 +0100

    Update commentRouter.js

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -10,3 +10,11 @@ router.get("/comments", checkAuth, async (req, res, next) => {
     res.status(500).json({ error: err.message });
   }
 });
+
+router.post("/comments", checkAuth, async (req, res, next) => {
+  try {
+    res.status(201).json(await Comment.create(req.body));
+  } catch (err) {
+    res.status(422).json({ error: err.message });
+  }
+});
